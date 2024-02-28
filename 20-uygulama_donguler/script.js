f22190693171fb34ea346b2aa9cf025231235bf0
onur-yasa <n.onur172@gmail.com>
2024-02-28 11:57:38 +0300

    js.

--- 20-uygulama_donguler/script.js
+++ 20-uygulama_donguler/script.js
@@ -1,4 +1,4 @@
-let sayilar = [1,5,7,15,3,25,12,24];
+let sayilar = [1, 5, 7, 15, 3, 25, 12, 24];
 
 // 1- sayilar listesindeki her bir elemanın karesini yazdırınız.
 
@@ -7,35 +7,35 @@ let sayilar = [1,5,7,15,3,25,12,24];
 // }
 
 // 2- sayilar listesindeki hangi sayılar 5' in katıdır?
-// for(let i=0; i<sayilar.length; i++) {
-//     if(sayilar[i] % 5 == 0) {
+// for(let i=0; i<sayilar.length; i++){
+//     if(sayilar[i] % 5==0){
 //         console.log(sayilar[i]);
 //     }
 // }
 
 // 3- sayilar listesindeki çift sayıların toplamını bulunuz.
 
-// let toplam = 0;
-// for(let i in sayilar) {
-//     if (sayilar[i]%2==0) {
+// let toplam=0;
+// for(let i in sayilar){
+//     if(sayilar[i]%2==0){
 //         toplam += sayilar[i];
 //     }
 // }
-// console.log(toplam);
+// console.log(toplam)
 
-let urunler = ["iphone 12","samsung s22","iphone 13","samsung s23","samsung s20"];
+//   let urunler = ["iphone 12","samsung s22","iphone 13","samsung s23","samsung s20"];
 
 // 4- urunler listesindeki tüm ürünleri büyük harf ile yazdırınız.
 
-// for(let urun of urunler) {
+// for(let urun of urunler){
 //     console.log(urun.toUpperCase());
 // }
 
 // 5- urunler listesinde samsung geçen kaç ürün vardır?
 
-// let adet = 0;
+// let adet=0;
 
-// for(let urun of urunler) {
+// for(let urun of urunler){
 //     if(urun.includes("samsung")) {
 //         adet++;
 //     }
@@ -44,33 +44,33 @@ let urunler = ["iphone 12","samsung s22","iphone 13","samsung s23","samsung s20"
 // console.log(adet);
 
 let ogrenciler = [
-    {"ad":"yiğit", "soyad": "bilgi", "notlar": [60,70,60]},
-    {"ad":"ada", "soyad": "bilgi", "notlar": [80,70,80]},
-    {"ad":"çınar", "soyad": "turan", "notlar": [10,20,60]}
+    { "ad": "yiğit", "soyad": "bilgi", "notlar": [60, 70, 60] },
+    { "ad": "ada", "soyad": "bilgi", "notlar": [80, 70, 80] },
+    { "ad": "çınar", "soyad": "turan", "notlar": [10, 20, 60] }
 ];
 
 // ogrenciler listesindeki her öğrencinin not ortalaması ve başarı durumlarını yazdırınız.
 
-for(let ogrenci of ogrenciler) {
+for (let ogrenci of ogrenciler) {
     let not_toplam = 0;
     let ortalama = 0;
     let adet = 0;
-    for(let not of ogrenci.notlar) {
+    for (let not of ogrenci.notlar) {
         not_toplam += not;
         adet++;
     }
     ortalama = not_toplam / adet;
-
-    console.log(`${ogrenci.ad} ${ogrenci.soyad} isimli öğrencimnin not ortalaması : ${ortalama}.`);
-
-    if(ortalama>=50) {
-        console.log("başarılı.");
+    console.log(`${ogrenci.ad} ${ogrenci.soyad} isimli öğrencinin not ortalaması : ${ortalama}.`)
+    if (ortalama >= 50) {
+        console.log("Başarılı")
     } else {
-        console.log("başarısız.");
-    }    
+        console.log("Bararısız.")
+    }
 }
 
-// tüm öğrencilerin not ortalaması kaçtır?
+
+
+// // tüm öğrencilerin not ortalaması kaçtır?
 let ogrenci_ortalamalar = [];
 for(let ogrenci of ogrenciler) {
 
